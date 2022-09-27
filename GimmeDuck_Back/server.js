9aace938a2c4188fd34e01a670ed24eb5fdf0457
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-09-27 14:25:58 +0900

    ipfs 실험중

--- GimmeDuck_Back/server.js
+++ GimmeDuck_Back/server.js
@@ -3,16 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const config = require('./config/key');
 const {User} = require('./models/User');
-const test = require('./Router/test');
-const hashlips = require('./Router/hashlips');
+//const test = require('./Router/test');
 
 var cors = require('cors')
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ limit : "10mb", extended: true , parameterLimit : 50000}));
+app.use(bodyParser.json({ limit : "10mb" , extended : true}))
+//app.use(express.limit('4M'));
 
-app.use("/test", test);
-app.use("/hashlips",hashlips);
+//app.use("/test", test);
 
 const mongooes = require('mongoose');
 mongooes.connect(config.mongoURI,{})
@@ -61,6 +60,79 @@ app.post('/login',(req,res)=>{
     });
 });
 
+const { Readable } = require('stream');
+var axios = require('axios');
+var FormData = require('form-data');
+
+
+const pinFileToIPFS = async function(image) {
+
+    var data = new FormData();
+    
+    const buffer = new Buffer.from(image, "base64");
+    const stream = Readable.from(buffer);
+
+    data.append('file', stream,"GimmeDuck.png");
+    data.append('pinataOptions', '{"cidVersion": 1}');
+    data.append('pinataMetadata', '{"name": "MyFile", "keyvalues": {"company": "Pinata"}}');
+
+    var config = {
+    method: 'post',
+    url: 'https://api.pinata.cloud/pinning/pinFileToIPFS',
+    headers: { 
+        pinata_api_key: '71084a474f5f077b9699',
+        pinata_secret_api_key: '5d9f0655f5b4194f2e0bfac0eff6fe0005a242c8af72bed24cf78cb61619d7da',
+        ...data.getHeaders()
+    },
+    data : data
+    };
+
+
+    const res = await axios(config);
+    console.log(res.data);
+    console.log('!!!!!!!!!!!!!!!!!!!start!!!!!!!!!!!!!!!!!!');
+
+
+    var data2 = JSON.stringify({
+        "pinataOptions": {
+          "cidVersion": 1
+        },
+        "pinataMetadata": {
+          "name": "Real_Test",
+          "keyvalues": {
+            "customKey": "customValue",
+            "customKey2": "customValue2"
+          }
+        },
+        "pinataContent": {
+            "name":"Gimme_duck_test",
+            "description":"Gimme_duck upload practice!",
+            "image":"ipfs://"+res.data.IpfsHash,
+            "attributes":[{"trait_type": "Unknown","value": "Unknown"}]
+        }
+      });
+      
+      var config2 = {
+        method: 'post',
+        url: 'https://api.pinata.cloud/pinning/pinJSONToIPFS',
+        headers: { 
+          'Content-Type': 'application/json', 
+          pinata_api_key: '71084a474f5f077b9699',
+            pinata_secret_api_key: '5d9f0655f5b4194f2e0bfac0eff6fe0005a242c8af72bed24cf78cb61619d7da'
+        },
+        data : data2
+      };
+
+      
+    const res2 = await axios(config2);
+    console.log(res2.data);
+
+} 
+
+app.post('/test', (req, res) => {
+    const image = req.body.image;
+    pinFileToIPFS(image);
+});
 
 
 const port = 8000; 
